3eaf5582cb31cef7809adb3fdac2d6d75ba3132a
Andrey Gonçalves <49600701+zAndreyG@users.noreply.github.com>
2020-05-06 20:09:34 -0300

    Adding Countries DropDown

--- covid19_info/lib/main.dart
+++ covid19_info/lib/main.dart
@@ -79,6 +79,39 @@ class HomeScreen extends StatelessWidget {
                 ],
               ),
             ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            height: 60,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(25),
+              border: Border.all(
+                color: Color(0xFFE5E5E5),
+              ),
+            ),
+            child: Row(
+              children: <Widget>[
+                SvgPicture.asset('assets/icons/maps-and-flags.svg'),
+                SizedBox(width: 20),
+                Expanded(child: DropdownButton(
+                  isExpanded: true,
+                  underline: SizedBox(),
+                  icon: SvgPicture.asset('assets/icons/dropdown.svg'),
+                  value: 'Indonésia',
+                  items: ['Indonésia', 'China', 'Estados Unidos', 'Japão']
+                    .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (value) {},
+                ),)
+              ],
+            ),
           )
         ],
       ),
